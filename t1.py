eb11ed63ba89abcfc972fbeed1e8961f6cd59f4e
Kostas Tsiamasiotis <konstantinos.Tsiamasiotis@iriworldwide.com>
2020-11-08 17:52:27 +0200

    Added README

--- t1.py
+++ t1.py
@@ -1,9 +1,24 @@
-import pandas as pd 
-import numpy as np 
-
-import gym
-
-env = gym.make('FrozenLake-v0')
+import pandas as pd
+import numpy as np
 
 
+class Stack(object):
 
+    def __init__(self):
+        self._stack = []
+
+    def __len__(self):
+        return len(self._stack)
+
+    def push(self, element):
+        self._stack.append(element)
+        
+    def pop(self):
+        try:
+            element = self._stack.pop()
+        except:
+            element = None
+        return element
+    
+    def empty(self):
+        self._stack = []
